e6536c0b256b60fdadcfcbad2676929c61a1c74c
Jordan Cartwright <48060710+bpmxjordy@users.noreply.github.com>
2025-02-06 00:58:12 +0000

    Added firebase auth and user login

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,4 @@
+
 const app = require('express')();
 const PORT = 8080;
 
@@ -5,3 +6,10 @@ app.listen(
     PORT,
     () => console.log(`it's alive on http://localhost:${PORT}`)
 )
+
+app.get('/rain' , async (req, res) => {
+    const responses = await ('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&format=flatbuffers');
+    res.status(200).send({
+        timezone: responses[0]
+    })
+})
